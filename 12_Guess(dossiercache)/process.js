c234099e6519135ec1547a20b7698a030615caa0
clemcheyrou <clemcheyrou@gmail.com>
2025-12-20 17:25:53 +0100

    fix pb

--- 12_Guess(dossiercache)/process.js
+++ 12_Guess(dossiercache)/process.js
@@ -4,4 +4,4 @@ const results = await readFile('./results.txt', 'utf-8')
 
 const lines = results.split('\n')
 
-console.log('line with numbers', lines.find((line) => /\d/.test(line)))
+console.log('result', lines.find((line) => /\d/.test(line)))
